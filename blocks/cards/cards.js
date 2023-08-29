79adbdbabe136fe6a706b21f12bb646efe095d25
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-08-29 12:07:28 +0530

    carosel added

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,6 +3,7 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
+  ul.className = 'cards-carousel'; // Add the carousel class here
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
@@ -12,10 +13,32 @@ export default function decorate(block) {
     });
     ul.append(li);
   });
+
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  
+  // Remove the existing content of the block
   block.textContent = '';
-  block.append(ul);
+
+  // Now add the carousel wrapper and navigation buttons
+  const carouselWrapper = document.createElement('div');
+  carouselWrapper.className = 'carousel-wrapper';
+  carouselWrapper.append(ul);
+
+  const prevButton = document.createElement('button');
+  prevButton.className = 'prev-btn';
+  prevButton.textContent = 'Previous';
+
+  const nextButton = document.createElement('button');
+  nextButton.className = 'next-btn';
+  nextButton.textContent = 'Next';
+
+  // Add the navigation buttons to the carousel wrapper
+  carouselWrapper.append(prevButton, nextButton);
+
+  // Finally, append the carousel wrapper to the block
+  block.append(carouselWrapper);
 }
+
 
 
 

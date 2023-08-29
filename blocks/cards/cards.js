e3f4b49ae37bb634db887f2785361557833f079e
yash14x2 <48061176+yash14x2@users.noreply.github.com>
2023-08-29 16:43:58 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -15,33 +15,33 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   
   // Remove the existing content of the block
-  block.textContent = '';
 
-  // Create a new carousel wrapper div
-  const carouselWrapper = document.createElement('div');
-  carouselWrapper.className = 'carousel-wrapper';
-
-  // Append the ul (with the generated li elements) to the carousel wrapper
-  carouselWrapper.append(ul);
-
-  // Create a new div to wrap the carousel wrapper
-  const outerWrapper = document.createElement('div');
-  outerWrapper.className = 'outer-wrapper';
-
-  // Append the carousel wrapper to the outer wrapper
-  outerWrapper.append(carouselWrapper);
-
-  // Append the outer wrapper to the block
-  block.append(outerWrapper);
-
-  // Set styles for the carousel wrapper
-  carouselWrapper.style.width = "100%"; // Set to desired width
-  carouselWrapper.style.height = "100%"; // Set to desired height
-  carouselWrapper.style.overflow = "hidden"; // Apply overflow hidden
 
   // Add more styles as needed for the outerWrapper and other elements
 }
+block.textContent = '';
 
+// Create a new carousel wrapper div
+const carouselWrapper = document.createElement('div');
+carouselWrapper.className = 'carousel-wrapper';
+
+// Append the ul (with the generated li elements) to the carousel wrapper
+carouselWrapper.append(ul);
+
+// Create a new div to wrap the carousel wrapper
+const outerWrapper = document.createElement('div');
+outerWrapper.className = 'outer-wrapper';
+
+// Append the carousel wrapper to the outer wrapper
+outerWrapper.append(carouselWrapper);
+
+// Append the outer wrapper to the block
+block.append(outerWrapper);
+
+// Set styles for the carousel wrapper
+carouselWrapper.style.width = "100%"; // Set to desired width
+carouselWrapper.style.height = "100%"; // Set to desired height
+carouselWrapper.style.overflow = "hidden"; // Apply overflow hidden
 const pictureElements = document.querySelectorAll('.default-content-wrapper picture');
 
 // Loop through each <picture> element and add the "slide" class

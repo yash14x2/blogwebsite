ec51905602f63b8d0d435306ace63eec0ab08ed6
vishaligave <142587786+vishaligave@users.noreply.github.com>
2023-08-31 19:23:42 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -108,3 +108,18 @@ function gonext() {
   counter++
   slideimage()
 }
+
+
+$(document).ready(function () {
+    $('.tekno-news-cards-wrapper').slick({
+        autoplaySpeed: 1000,
+        speed : 1000,
+        autoplay: true,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 3,
+        centerMode: true,
+        variableWidth: true,
+        arrows: false,
+     });
+});

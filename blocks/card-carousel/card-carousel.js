86294da77f0c7c4c986285f5a2fa4699102a0db1
Vishal Igave <vishal.igave@teknopoint.in>
2023-08-31 19:11:28 +0530

    slick code added

---
+++ blocks/card-carousel/card-carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.tekno-news-cards-wrapper').slick({
+        autoplaySpeed: 1000,
+        speed : 1000,
+        autoplay: true,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 3,
+        centerMode: true,
+        variableWidth: true,
+        arrows: false,
+     });
+});

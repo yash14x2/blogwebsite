9bc3f469f5dcce9c7abe60fa90c1a534a2fa6487
Vishal Igave <vishal.igave@teknopoint.in>
2023-09-01 16:38:54 +0530

    tiny slider lib integrate

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -108,17 +108,3 @@ function gonext() {
   counter++
   slideimage()
 }
-
-
-$(document).ready(function () {
-  $(".tekno-news-cards-wrapper").find(".cards ul").slick({
-      speed : 1000,
-      autoplay: false,
-      infinite: true,
-      speed: 3000,
-      slidesToShow: 3,
-      centerMode: true,
-      variableWidth: true,
-      arrows: false,
-   });
-});

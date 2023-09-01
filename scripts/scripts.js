98d0da79304bb48f018e9da8bb1a6e502f618504
vishaligave <142587786+vishaligave@users.noreply.github.com>
2023-09-01 16:04:44 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -133,16 +133,3 @@ async function loadPage() {
 }
 
 loadPage();
-
-$(document).ready(function () {
-  $(".tekno-news-cards-wrapper").find(".cards-wrapper .cards ul").slick({
-      speed : 1000,
-      autoplay: false,
-      infinite: true,
-      speed: 3000,
-      slidesToShow: 3,
-      centerMode: true,
-      variableWidth: true,
-      arrows: false,
-   });
-});

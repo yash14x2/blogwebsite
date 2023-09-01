90bcae2ba82a0fd7e6724c44cc27adbb5863c318
vishaligave <142587786+vishaligave@users.noreply.github.com>
2023-09-01 13:58:45 +0530

    Update card-carousel.js

--- blocks/card-carousel/card-carousel.js
+++ blocks/card-carousel/card-carousel.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.tekno-news-cards-wrapper .default-content-wrapper .cards-wrapper .cards ul').slick({
+    $('.tekno-news-cards-wrapper ul').slick({
         speed : 1000,
         autoplay: false,
         infinite: true,

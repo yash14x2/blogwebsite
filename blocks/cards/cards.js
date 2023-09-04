59ae2702943107a03f16d8d734ecc074082da1ac
vishaligave <142587786+vishaligave@users.noreply.github.com>
2023-09-04 18:08:51 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -112,23 +112,43 @@ function gonext() {
 
 
 
-document.addEventListener("DOMContentLoaded", function () {
-const ulElement = document.querySelector('.tekno-news-cards-wrapper .cards-wrapper ul');
-console.log(ulElement);
-  // import { tns } from "./node_modules/tiny-slider/src/tiny-slider";
- 
+let sliderInitialized = false; // To track whether the slider has been initialized
+
+// Function to initialize the slider
+function initializeSlider() {
+  const ulElement = document.querySelector('.tekno-news-cards-wrapper .cards-wrapper ul');
+
+  if (ulElement && !sliderInitialized) {
+    console.log('Initializing slider...');
 
     const slider = tns({
       container: '.tekno-news-cards-wrapper .cards-wrapper .cards ul',
       loop: true,
       items: 1,
       slideBy: 'page',
-      nav: false,    
+      nav: false,
       autoplay: true,
       speed: 400,
-      autoplayButtonOutput: false, 
+      autoplayButtonOutput: false,
       controls: false,
       mouseDrag: true,
       lazyload: true,
     });
-});
+
+    sliderInitialized = true; // Set to true to avoid reinitialization
+  }
+}
+
+// Function to check if the user has scrolled to a certain point
+function checkScroll() {
+  const triggerPoint = 300; // Adjust this value as needed
+
+  if (window.scrollY > triggerPoint) {
+    initializeSlider();
+    // Remove the scroll event listener to prevent reinitialization
+    window.removeEventListener('scroll', checkScroll);
+  }
+}
+
+// Add a scroll event listener to trigger the initialization when the user scrolls
+window.addEventListener('scroll', checkScroll);
